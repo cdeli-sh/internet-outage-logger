7e95949866b06618591be11cfe08f82405f68bdb
cdellies <celien.aupiais@molecul.fr>
2024-03-03 21:32:12 +0100

    Add initial project files and dependencies

---
+++ index.js
@@ -0,0 +1,45 @@
+import 'dotenv/config';
+import isOnline from 'is-online';
+import fetch from 'node-fetch'
+
+let isInternetDown = false;
+let timeDown = 0;
+let timeDownStart = 0;
+let timeDownEnd = 0;
+
+let discordWebhookUrl = process.env.DISCORD_WEBHOOK_URL;
+
+function sendDiscordMessage(message) {
+  fetch(discordWebhookUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ content: message, username: 'Internet Monitor', avatar_url: 'https://i.imgur.com/4M34hi2.png' })
+  });
+}
+
+setInterval(() => {
+  isOnline().then(online => {
+    if (online) {
+      if (isInternetDown) {
+        timeDownEnd = Date.now();
+        timeDown = timeDownEnd - timeDownStart;
+        console.log('Internet is back up');
+        console.log('Time down: ', timeDown);
+        if (timeDown > 1000) {
+          sendDiscordMessage('Internet was down for ' + timeDown / 1000 + 's');
+        }
+        isInternetDown = false;
+        timeDownStart = 0;
+        timeDownEnd = 0;
+      }
+    } else {
+      if (!isInternetDown) {
+        timeDownStart = Date.now();
+        console.log('Internet is down');
+        isInternetDown = true;
+      }
+    }
+  });
+}, 1000);
---
+++ service-manager.js
@@ -0,0 +1,24 @@
+var Service = require('node-windows').Service;
+
+var svc = new Service({
+  name: 'Internet Monitor',
+  description: 'Monitors the internet and sends a message to Discord when it goes down or comes back up',
+  script: 'C:\\Users\\delli\\Documents\\internet-outage-logger\\index.js'
+});
+
+svc.on('install', function () {
+  svc.start();
+});
+
+svc.on('uninstall', function () {
+  console.log('Uninstall complete.');
+  console.log('The service exists: ', svc.exists);
+});
+
+if (process.argv[2] === 'install') {
+  svc.install();
+  console.log('Service installed');
+} else if (process.argv[2] === 'uninstall') {
+  svc.uninstall();
+  console.log('Service uninstalled');
+}
